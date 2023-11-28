1087f2fb2b879777c7c15d908168d37b071b0205
minhphat <56902484+minhphat2000@users.noreply.github.com>
2023-11-28 10:00:39 +0700

    Update human.py

--- human detection/human.py
+++ human detection/human.py
@@ -1,4 +1,10 @@
 from person_height_estimator import estimate_person_height
+import cv2
 
-# Call the function with your desired parameters
-estimate_person_height(camera_index=0, known_distance=2.0, known_height=1.7)
+if __name__ == "__main__":
+    # Call the function with your desired parameters for each camera
+    cap_1 = cv2.VideoCapture(0)
+    cap_2 = cv2.VideoCapture(1)
+
+    estimate_person_height(cap_1, known_distance=2.0, known_height=1.7)
+    estimate_person_height(cap_2, known_distance=2.0, known_height=1.7)
